d5f5d6cd97838aff5dd52a60b461c65a98c727c2
k4itrun <103044629+k4itrun@users.noreply.github.com>
2023-12-10 17:37:34 -0500

    new build

---
+++ start.bat
@@ -0,0 +1,32 @@
+@echo off
+
+if exist ".\node_modules\" if exist ".\package-lock.json" (
+    npm run start
+
+    if %errorlevel% neq 0 (
+        echo Error: An error occurred while starting.
+        exit /b %errorlevel%
+    ) else (
+        echo Started successfully.
+    )
+) else (
+    echo Installing dependencies...
+    npm install
+
+    if %errorlevel% neq 0 (
+        echo Error: Failed to install dependencies.
+        exit /b %errorlevel%
+    ) else (
+        echo Correctly installed dependencies. Running the program...
+        npm run start
+
+        if %errorlevel% neq 0 (
+            echo Error: An error occurred while starting.
+            exit /b %errorlevel%
+        ) else (
+            echo Started successfully.
+        )
+    )
+)
+
+pause
