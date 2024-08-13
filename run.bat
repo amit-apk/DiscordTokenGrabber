b1349235a539b5d01f23caed509faa6b890b1da8
k4itrun <103044629+k4itrun@users.noreply.github.com>
2024-08-13 11:56:28 -0500

    new version

--- run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-node builder.js
--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    WEBHOOK: ["%WEBHOOK%"],
-    ERROR_MESSAGE: "%ERROR_MESSAGE%",
-    VM_DEBUGGER: "%VM_DEBUGGER%",
-    DC_INJECTION: "%DC_INJECTION%"
-};
-
-// if you want something better, you can contact me, DIsocrd: k4itrunnnssh
